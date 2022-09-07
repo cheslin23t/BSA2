bd3c71df08cab79814849993169555503f343401
Liam Sholts <67148623+cheslin23t@users.noreply.github.com>
2022-09-07 17:24:42 -0400

    isgoingform

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
+def index():
     return render_template('index.html')
 
+@app.route('/isgoingform', methods = ['GET'])
+def isgoingform():
+    return render_template('isgoingform.html')
 
+@app.route('/isgoingform', methods = ['POST'])
+def isgoingformpost():
+    print(request.form)
+    return request.form
+
+@app.route('/static/<path:path>')
+def send_report(path):
+    return send_from_directory('static', path)
 app.run(host='0.0.0.0', port=81)
